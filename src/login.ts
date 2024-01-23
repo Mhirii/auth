d2e410e05da9bea484ba2eaaa53f0ca68fd23709
Mhirii <ahmedmhiri218@gmail.com>
2024-01-23 18:02:39 +0100

    login

--- src/login.ts
+++ src/login.ts
@@ -1,9 +1,78 @@
 import { Context } from "hono";
+import {
+  compare,
+  createAccessToken,
+  createRefreshToken,
+  getXata,
+} from "./utils";
 
 export const login = async (c: Context) => {
   const body = await c.req.json();
-  // TODO: get user from DB
-  // TODO: check password hash
-  // TODO: Generate new Session
-  return c.json(body);
+
+  const { email, password } = await c.req.json();
+
+  const apiKey = await c.env.XATA_API_KEY;
+  const branch = await c.env.XATA_BRANCH;
+
+  try {
+    const xata = getXata(apiKey, branch);
+    const secret = await c.env.JWT_SECRET;
+
+    const users = await xata.db.auths.filter({ email: email }).getMany();
+    if (!users[0]) {
+      return c.json({ error: "User not found" });
+    }
+    const { id, password_hash, username } = users[0];
+    if (!password_hash || !id || !username) {
+      return c.json({ error: "User not found" });
+    }
+
+    const match = await compare(password, password_hash);
+    if (!match) {
+      return c.json({ error: "Invalid password" });
+    }
+
+    const { accessToken, accessTokenExpiry } = await createAccessToken(secret, {
+      username: username,
+      email: email,
+      id: id,
+    });
+
+    // if the access token already exists then delete the session
+    const sessions = await xata.db.sessions
+      .filter({ access_token: accessToken })
+      .getMany();
+    if (sessions[0]) {
+      await xata.db.sessions.delete(sessions[0].id);
+    }
+
+    const { refreshToken } = await createRefreshToken(secret, { id: id });
+
+    const access_token_expires = new Date(accessTokenExpiry);
+
+    const session = await xata.db.sessions.create({
+      auth: id,
+      refresh_token: refreshToken,
+      access_token: accessToken,
+      access_token_expires,
+      platform: "",
+    });
+
+    const response = {
+      id: id,
+      username,
+      email,
+      verified: users[0].verified,
+      session: {
+        id: session.id,
+        access_token: accessToken,
+        refresh_token: refreshToken,
+        access_token_expiry: accessTokenExpiry.toString(),
+      },
+    };
+
+    return c.json(response);
+  } catch (error) {
+    return c.json(error);
+  }
 };
